cd2ba7eda507bb76553d6c3f57f0c230dc3baaa8
LuisReyes367 <28022004luismario@gmail.com>
2025-07-14 10:22:33 -0600

    se modifico una vez mas la estroctura del carrusel[B

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,67 +1,13 @@
 import React from 'react';
-import { Image, Platform, StyleSheet, View } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, View } from 'react-native';
 import { Navbar } from '@/components/Navbar';
 import Carousel from '@/components/carrusel';
 
 export default function HomeScreen() {
   return (
-    
     <View style={styles.mainContainer}>
       <Navbar />
-      
-      <ParallaxScrollView
-        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-        headerImage={
-          <Image
-            source={require('@/assets/images/partial-react-logo.png')}
-            style={styles.reactLogo}
-          />
-        }
-
-        
-        
-      >
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">Welcome to Gymso Fitness!</ThemedText>
-          <HelloWave />
-        </ThemedView>
-        <Carousel/>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-          <ThemedText>
-            Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-            Press{' '}
-            <ThemedText type="defaultSemiBold">
-              {Platform.select({
-                ios: 'cmd + d',
-                android: 'cmd + m',
-                web: 'F12',
-              })}
-            </ThemedText>{' '}
-            to open developer tools.
-          </ThemedText>
-        </ThemedView>
-              
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          <ThemedText>
-            {`Tap the Explore tab to learn more about what's included in this starter app.`}
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-          <ThemedText>
-            {`When you're ready, run `}
-            <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-            <ThemedText type="defaultSemiBold">app</ThemedText> directory.
-          </ThemedText>
-        </ThemedView>
-      </ParallaxScrollView>
+      <Carousel />
     </View>
   );
 }
@@ -70,26 +16,5 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  scrollContent: {
-    paddingTop: 70, 
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-    marginBottom: 20,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 20,
-    paddingHorizontal: 15,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
