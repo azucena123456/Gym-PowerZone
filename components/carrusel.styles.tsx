1856e4baaac0b2c614257019d950ae399431100c
Azucena <azuhernandez226@gmail.com>
2025-07-15 23:22:42 -0600

    Cambios en el carrusel

--- components/carrusel.styles.tsx
+++ components/carrusel.styles.tsx
@@ -1,22 +1,21 @@
-import { StyleSheet,Platform } from 'react-native';
+import { StyleSheet, Platform, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const CARD_WIDTH = width * 0.25;
+const CARD_HEIGHT = 450;
 
 export const styles = StyleSheet.create({
   fullWidthContainer: {
-
-    
     width: '100%',
     backgroundColor: '#EEEEEE',
-    paddingVertical: 10,
-    paddingHorizontal:65,
-    
-    
+    paddingVertical: 80,
+    paddingHorizontal: 50,
   },
-nombreEntrenador: {
-  color: '#333333',  
-  fontWeight: 'bold', 
-},
-
-
+  nombreEntrenador: {
+    color: '#333333',
+    fontWeight: 'bold',
+  },
   headerTitle: {
     fontSize: Platform.select({ ios: 20, android: 20, default: 20 }),
     fontWeight: 'bold',
@@ -24,21 +23,26 @@ nombreEntrenador: {
     marginBottom: 15,
     color: '#818181',
   },
-  headerTitle1:{
+  headerTitle1: {
     fontSize: Platform.select({ ios: 20, android: 20, default: 28 }),
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 30,
-    marginTop:-15,
+    marginTop: -15,
     color: '#121212',
   },
-
   carouselContent: {
-    paddingLeft: Platform.select({ ios: 15, android: 15, default: 300 }),
-    paddingRight: Platform.select({ ios: 5, android: 5, default: 10 }),
+    paddingHorizontal: Platform.select({
+      ios: 40,
+      android: 40,
+      default: 100,
+    }),
+    alignItems: 'center',
   },
   slide: {
-    marginRight: Platform.select({ ios: 15, android: 15, default: 25 }),
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    marginHorizontal: 10,
     backgroundColor: '#F9F9F9',
     borderRadius: 0,
     overflow: 'hidden',
@@ -47,11 +51,10 @@ nombreEntrenador: {
     shadowOpacity: 0.31,
     shadowRadius: 8,
     elevation: 6,
-    
-
   },
   image: {
     width: '100%',
+    height: '60%',
     borderTopLeftRadius: 0,
     borderTopRightRadius: 0,
   },
@@ -62,19 +65,21 @@ nombreEntrenador: {
     borderWidth: 1,
     borderColor: '#0000',
     borderTopWidth: 10,
+    flex: 1,
+    justifyContent: 'space-between',
   },
   mobileTextContainer: {
     padding: 10,
   },
   titleContainer: {
     width: '100%',
-    
     marginBottom: 8,
   },
   contentWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
+    flex: 1,
   },
   textContent: {
     flex: 1,
@@ -82,10 +87,8 @@ nombreEntrenador: {
   },
   slideTitle: {
     fontSize: Platform.select({ ios: 16, android: 16, default: 25 }),
-    
     fontWeight: 'bold',
     color: '#333',
-    
   },
   mobileSlideTitle: {
     fontSize: 14,
@@ -107,43 +110,24 @@ nombreEntrenador: {
   mobileSlideDescription: {
     fontSize: 12,
   },
-  ctaButton: {
+  ctaCircle: {
     backgroundColor: '#F13a11',
-    paddingVertical: Platform.select({ ios: 8, android: 8, default: 10 }),
-    paddingHorizontal: Platform.select({ ios: 12, android: 12, default: 15 }),
-    borderRadius: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
     alignSelf: 'center',
+    marginTop: -100,
   },
-  mobileCtaButton: {
-    paddingVertical: 6,
-    paddingHorizontal: 10,
-    borderRadius: 15,
+  mobileCtaCircle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
- 
-
-  
-ctaCircle: {
-  backgroundColor: '#F13a11', 
-  width: 50,                  
-  height: 50,                 
-  borderRadius: 30,           
-  justifyContent: 'center',
-  alignItems: 'center',
-  alignSelf: 'center',   
-  marginTop: -100     
-},
-
-mobileCtaCircle: {
-  width: 50,
-  height: 50,
-  borderRadius: 25,
-},
-
-
-ctaText: {
-  color: 'white',
-  fontWeight: '600',
-  fontSize: Platform.select({ ios: 12, android: 12, default: 14 }),
-},
-  
+  ctaText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: Platform.select({ ios: 12, android: 12, default: 14 }),
+  },
 });
