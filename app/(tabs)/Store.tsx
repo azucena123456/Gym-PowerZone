ae0071dc2997fdb897ecd348a569c51ec18c481b
LuisReyes367 <28022004luismario@gmail.com>
2025-08-07 17:51:57 -0600

    se modifico la lelelnda para que se muestre de color rojo y la palabra buscada de color rojo bont

--- app/(tabs)/Store.tsx
+++ app/(tabs)/Store.tsx
@@ -81,7 +81,9 @@ export default function StoreScreen() {
 
                 {filteredProducts.length === 0 && (
                     <View style={styles.noResultsContainer}>
-                        <Text style={styles.noResultsText}>No se encontraron resultados para {searchTerm}</Text>
+                        <Text style={styles.noResultsText}>No se encontraron resultados para<Text style = {{ fontWeight: 'bold'}}> {searchTerm}</Text>
+
+                        </Text>
                     </View>
                 )}
 
